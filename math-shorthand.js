93c2b88555a7dca4161281516344e2dbc1549a19
Sobuj <sobujkumar424@gmail.com>
2022-01-26 12:01:52 +0600

    added math-shorthand

---
+++ math-shorthand.js
@@ -0,0 +1,49 @@
+var price1 = 20;
+var price2 = 15;
+var sum = price1 + price2;
+var difference = price2 - price1;
+var multiplication = price2 * price1;
+var division = price1 / price2;
+
+console.log(sum);
+console.log(difference);
+console.log(multiplication);
+console.log(division);
+
+//
+
+var doublePrice = price2 * 2;
+var newPrice = price1 + 15;
+
+console.log(newPrice);
+console.log(doublePrice);
+
+//  Javascript shorthand
+
+var price1 = price1 + 15;
+price1 = price1 + 15;
+price1 += 15;
+console.log(price1);
+
+var price2 = price2 - 15;
+price2 = price2 - 15;
+price2 -= 15;
+console.log(price2);
+
+var agg = 14;
+agg = agg + 10;
+agg += 10;
+console.log(agg);
+
+var agg = 14;
+agg = agg + 1;
+agg += 1;
+agg++;
+console.log(agg);
+
+var price = 100;
+price = price - 1;
+price -= 1;
+price--;
+console.log(price);
+
