6482a4bbb790edafc8e84195e5d97f94063647fe
Sobuj <sobujkumar424@gmail.com>
2022-01-26 12:47:18 +0600

    added concatenation

---
+++ concatenation.js
@@ -0,0 +1,48 @@
+var firstName = "Sobuj";
+var lastName = "Mondol";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+var firstName = "Bithi";
+var lastName = "Biswash";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// integer
+var number = 5;
+// float
+var price = 10.10;
+
+// String valu
+var tomatoPrice = "30";
+var eggPrice = "10";
+var totalPrice = tomatoPrice + eggPrice;
+console.log(totalPrice);
+
+// string theke number a convert kora holo (+)
+var tomatoPrice = +"30";
+var eggPrice = +"10";
+var totalPrice = tomatoPrice + eggPrice;
+console.log(totalPrice);
+
+
+var tomatoPrice = "30";
+var eggPrice = "10";
+var tomatoPriceNumber = parseInt(tomatoPrice);
+var eggPriceNumber = parseInt(eggPrice);
+var totalPrice = tomatoPriceNumber + eggPriceNumber;
+console.log(tomatoPriceNumber);
+console.log(eggPriceNumber);
+console.log(totalPrice);
+
+
+var tomatoPrice = "30.18";
+var eggPrice = "10.15";
+var tomatoPriceNumber = parseFloat(tomatoPrice);
+var eggPriceNumber = parseFloat(eggPrice);
+var totalPrice = tomatoPriceNumber + eggPriceNumber;
+console.log(tomatoPriceNumber);
+console.log(eggPriceNumber);
+console.log(totalPrice);
+ 
