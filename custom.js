c6dfc91083f007cd0ed9338161e5bb5c7174c828
Sobuj <sobujkumar424@gmail.com>
2022-01-26 11:21:20 +0600

    Javascript

---
+++ custom.js
@@ -0,0 +1,29 @@
+// string
+var name = "sobuj";
+console.log(name);
+// bulian
+var name = teue;
+console.log(name);
+// number
+var number = 12345;
+console.log(number);
+// Price
+var onionPrice = 45;
+console.log(onionPrice);
+var eggPrice = 10;
+console.log(eggPrice);
+
+//  math + - / *
+  
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+
+var priceDifference = onionPrice - eggPrice;
+clonsole.log(priceDifference);
+
+var priceMultiplication = onionPrice * eggPrice;
+console.log(priceMultiplication);
+
+var priceDivide = onionPrice / eggPrice;
+console.log(priceDivide);
+
