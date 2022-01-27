1a375da38d549250f7fdf1f51a20150df2a3902f
Sobuj <sobujkumar424@gmail.com>
2022-01-27 19:32:51 +0600

    added conditional.js

---
+++ array.js/conditional.js
@@ -0,0 +1,24 @@
+var iphonePrice = 150000;
+var myBudget = 10000;
+
+if (iphonePrice < myBudget){
+    console.log("tahole ami iphone kinbo");
+}else{
+    console.log("na hole amar shaomi valo");
+}
+
+// if (iphonePrice > myBudget){
+//     console.log("tahole ami iphone kinbo");
+// }else{
+//     console.log("tahole iphone tumi futo");
+// }
+
+// if (iphonePrice == myBudget){
+//     console.log("tahole ami iphone kinbo");
+// }else{
+//     console.log("Tahole iphone tumi futo");
+// }
+
+// if (iphonePrice != myBudget){
+//     console.log("tahole ami iphone kinbo");
+// }
