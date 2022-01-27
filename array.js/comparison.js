d95122f404753ccf598abb4e246f7c9a42985505
Sobuj <sobujkumar424@gmail.com>
2022-01-27 19:04:11 +0600

    added comparison.js

---
+++ array.js/comparison.js
@@ -0,0 +1,19 @@
+console.log(10 > 15);
+console.log(10 < 15);
+console.log(10 == 15);
+console.log(15 == 15);
+console.log(15 <= 15);
+console.log(10 >= 15);
+console.log(15 != 15);
+
+var onionPraice = 50;
+var tomatoPrice = 40;
+
+console.log(onionPraice > tomatoPrice)
+console.log(onionPraice < tomatoPrice);
+console.log(onionPraice == tomatoPrice);
+console.log(onionPraice != tomatoPrice);
+
+// And / or 
+console.log(studyGood && goodPerson);
+console.log(isBanker || goodResult);
