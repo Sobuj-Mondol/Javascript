46716282d955f92f5db72e4122d7084d3476a96c
Sobuj <sobujkumar424@gmail.com>
2022-01-27 12:49:55 +0600

    added array.js

---
+++ array.js/array.js
@@ -0,0 +1,15 @@
+var friendsAge = [20, 18, 15];
+
+var coffeFee = [30, 40, 50, 100];
+
+var exLove = ["nodi", "shati", "lokkhi", "bithi"];
+
+var vowels = ["a", "e", "i", "o", "u"];
+
+console.log(exLove);
+
+// koyta Element ace ta ber kora
+
+var vowelsCount = vowels.length;
+console.log(vowelsCount);
+
