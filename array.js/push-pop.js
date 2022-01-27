8ea766c90123e8f0814dc1bd1ac32b2225f7ded3
Sobuj <sobujkumar424@gmail.com>
2022-01-27 16:02:51 +0600

    push-pop.js

---
+++ array.js/push-pop.js
@@ -0,0 +1,23 @@
+// add Elements to an array push
+var lastBench = ["kamal", "salam", "jamal", "korim"];
+lastBench.push("montu");
+lastBench.push("pannu");
+lastBench.push("mintu");
+lastBench.push("bolod");
+
+// console.log(lastBench);
+//
+var friendAge = [11, 13, 15, 20];
+friendAge.push(25);
+friendAge.push(22);
+friendAge.push(24);
+friendAge.push(30);
+
+// console.log(friendAge);
+//
+// remove an Element from an array pop
+var friendAge = [11, 13, 15, 20];
+friendAge.pop();
+friendAge.pop();
+friendAge.pop();
+console.log(friendAge);
