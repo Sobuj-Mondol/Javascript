8e07ba8a5aa8e4197d8e15f95e1f8daa422ffec0
Sobuj <sobujkumar424@gmail.com>
2022-01-27 19:54:46 +0600

    added maltipul-conditional.js

---
+++ array.js/maltipul-conditional.js
@@ -0,0 +1,30 @@
+// var gotJob = true;
+var gotJob = false;
+var moneySaved = 200000;
+
+if(godJob == true && moneySaved > 300000){
+    console.log('We are go to piknik');
+}
+else {
+    console.log('Otherwise we are kill you');
+}
+
+var gotJob = false;
+var moneySaved = 200000;
+
+if(godJob == true && moneySaved > 200000){
+    console.log('We are go to piknik');
+}
+else {
+    console.log('Otherwise we are kill you');
+}
+
+var gotJob = false;
+var moneySaved = 200000;
+
+if(godJob == true || moneySaved > 300000){
+    console.log('We are go to piknik');
+}
+else {
+    console.log('Otherwise we are kill you');
+}
