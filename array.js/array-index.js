f22b693b9246f1c1227784be38c5e79c7864a840
Sobuj <sobujkumar424@gmail.com>
2022-01-27 13:54:43 +0600

    added array-index

---
+++ array.js/array-index.js
@@ -0,0 +1,8 @@
+var teach = ["hablu", "bolod", "songodose", "sovab", "nosto"];
+
+var numbers = [1, 2, 3, 4, 5];
+
+var names = ["Sobuj", "Akash", "Raju", "bithi", "Nodi"];
+
+var index = names.indexOf(1);
+console.log(index);
