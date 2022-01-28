51a294832334138e4e4c51f764d6364e80437043
Sobuj <sobujkumar424@gmail.com>
2022-01-28 22:23:14 +0600

    added for-loop

---
+++ recap-loop/for-loop.js
@@ -0,0 +1,16 @@
+var penGiven = 0;
+while(penGiven <= 10){
+    console.log('give me a pen please');
+    penGiven++;
+}
+
+// for loop
+for (var penGiven =0; penGiven <= 10; penGiven++){
+    console.log('give me a pen please');
+    console.log(penGiven);
+}
+
+// for loop shorthand
+for(var i = 0; i <= 10; i++){
+    console.log(penGiven);
+}
