94cfaa7291b1db29355bd5d75be6454ca6b9e5cb
Sobuj <sobujkumar424@gmail.com>
2022-01-28 21:54:40 +0600

    while-loop-example

---
+++ recap-loop/while-loop-example.js
@@ -0,0 +1,19 @@
+var number  = 1;
+while(number <= 50){
+    console.log(number);
+    number++;
+}
+
+// odd numbers
+while(number <= 50){
+    console.log(number);
+    number = number+2;
+}
+
+// even numbers
+var number = 0;
+
+while(number <= 50){
+    console.log(number);
+    number = number +2;
+}
