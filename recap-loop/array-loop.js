cf626bbad101439b1721888b9da762fff4eda0ce
Sobuj <sobujkumar424@gmail.com>
2022-01-28 22:41:14 +0600

    added array-loop

---
+++ recap-loop/array-loop.js
@@ -0,0 +1,11 @@
+var number = [10, 20, 30, 40, 50];
+for(var i =0; i <= 50; i++){
+    var element = numbers[i];
+    console.log(element);
+}
+
+var number = [10, 20, 30, 40, 50, 60, 70];
+for(var i =0; i <= number.length; i++){
+    var element = numbers[i];
+    console.log(element);
+}
