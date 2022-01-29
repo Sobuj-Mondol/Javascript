f64620b9830eeb3406159c937571219d949d2ecd
Sobuj <sobujkumar424@gmail.com>
2022-01-29 19:03:05 +0600

    added switch

---
+++ explor-function-object/switch.js
@@ -0,0 +1,30 @@
+var color = 'black';
+
+if(color == 'blue'){
+    console.log('color is blue');
+}
+else if(color == 'red'){
+    console.log('color is red');
+}
+else if(color == 'black'){
+    console.log('color is black');
+}
+else{
+    console.log('no color');
+}
+
+
+switch (color){
+    case 'blue':
+        console.log('color is blue');
+        break;
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'black':
+        console.log('color is black');
+        break;
+    case 'yellow':
+        console.log('color is yellow');
+        break;
+}
