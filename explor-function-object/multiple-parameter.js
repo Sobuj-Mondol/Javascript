701cb30a69e2afaeb019384775ab54dc2e030c14
Sobuj <sobujkumar424@gmail.com>
2022-01-29 12:42:10 +0600

    added multiple-parameter

---
+++ explor-function-object/multiple-parameter.js
@@ -0,0 +1,46 @@
+function addTwoNumbers(number1, number2){
+    console.log(number1, number2)
+    var total = number1 + number2;
+    return total;
+}
+// var number1 = 20;
+// var number2 = 50;
+// var result = addTwoNumbers(number1, number2);
+var result = addTwoNumbers(20, 50);
+console.log('result value: ', result);
+
+
+function addTwoNumbers(number1, number2){
+    console.log(number1, number2)
+    var total = number1 - number2;
+    return total;
+}
+// var number1 = 20;
+// var number2 = 50;
+// var result = addTwoNumbers(number1, number2);
+var result = addTwoNumbers(20, 50);
+console.log('result value: ', result);
+
+// multiple two number
+function addTwoNumbers(number1, number2){
+    console.log(number1, number2)
+    var total = number1 * number2;
+    return total;
+}
+// var number1 = 20;
+// var number2 = 50;
+// var result = addTwoNumbers(number1, number2);
+var result = addTwoNumbers(20, 50);
+console.log('result value: ', result);
+
+// divag two number
+function addTwoNumbers(number1, number2){
+    console.log(number1, number2)
+    var total = number1 / number2;
+    return total;
+}
+// var number1 = 20;
+// var number2 = 50;
+// var result = addTwoNumbers(number1, number2);
+var result = addTwoNumbers(20, 50);
+console.log('result value: ', result);
