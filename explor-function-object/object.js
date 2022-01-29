00f0afe0c129b626f2a645f85da54ab5d05fc0fe
Sobuj <sobujkumar424@gmail.com>
2022-01-29 13:03:39 +0600

    added object

---
+++ explor-function-object/object.js
@@ -0,0 +1,19 @@
+var student1 = {
+    name: 'Sobuj',
+    roll: 10,
+    class: 'ten',
+    id: 156568762,
+};
+
+var mobile = {
+    color: 'white',
+    price:10000,
+    strong: '16gb',
+    rem: '2gb',
+};
+
+var sunglase = {
+    branch: 'puma',
+    color: 'black or red',
+    price: 50,
+}
