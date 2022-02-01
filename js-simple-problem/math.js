5a4120c10e5fc6008abee472acef44ff709c09b0
Sobuj <sobujkumar424@gmail.com>
2022-02-01 11:21:34 +0600

    added js-simple-problem

---
+++ js-simple-problem/math.js
@@ -0,0 +1,25 @@
+ const myNumber = -6;
+ const output = Math.ceil(myNumber);
+//
+const myNumber = 26.34561;
+const output = Math.ceil(myNumber);
+//
+const output = Math.floor(myNumber);
+//
+const output = Math.round(myNumber);
+//
+const output = Math.random() * 10;
+const round = Math.floor(output);
+//
+const round = Math.round(output);
+console.log(round);
+
+
+// for loop
+for(let i = 0; i <= 50; i++){
+    const output = Math.random() * 10;
+    const round = Math.floor(output);
+    //
+    const round = Math.round(output);
+    console.log(round);
+}
