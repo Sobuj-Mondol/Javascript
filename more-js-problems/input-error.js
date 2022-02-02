a683687054a988c27c193cc4309f1f761d76d499
Sobuj <sobujkumar424@gmail.com>
2022-02-02 15:24:02 +0600

    added input-error

---
+++ more-js-problems/input-error.js
@@ -0,0 +1,13 @@
+function multiple(num1,num2){
+    let sum = num1 + num2;
+    console.log(sum);
+}
+multiple(10,15);
+//
+
+function multiple(num1,num2){
+    let multi = num1 * num2;
+    console.log(multi);
+}
+multiple(10,15);
+ 
